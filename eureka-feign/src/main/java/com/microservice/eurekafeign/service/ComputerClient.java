0b987bd61493d8e6ef67471bfb97062518598882
JesonZHY <503816801@qq.com>
2019-04-01 17:19:57 +0800

    新增ribbon模块 feign模块

--- eureka-feign/src/main/java/com/microservice/eurekafeign/service/ComputerClient.java
+++ eureka-feign/src/main/java/com/microservice/eurekafeign/service/ComputerClient.java
@@ -1,11 +1,13 @@
 package com.microservice.eurekafeign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("computer-server")
+@Service
 public interface ComputerClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/calc")
